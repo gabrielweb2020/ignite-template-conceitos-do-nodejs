7303aae10ae41da91f17817a924940ea37d9e14c
Gabriel Bittencourt <gabriel.bc88@gmail.com>
2021-03-24 09:46:08 -0300

    refactor: refactoring the routes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const { v4: uuidv4 } = require('uuid');
+const { request, response } = require('express');
 
 const app = express();
 
@@ -11,37 +12,36 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  const { username } = request.headers
+  const { username } = request.headers;
 
-    const user = users.find(customer => customer.username === username)
+  const user = users.find(user => user.username === username);
 
-    if(!user) {
-        return response.status(400).json({ error: 'User not found!' })
-    }
+  if(!user){
+    return response.status(404).json({error: "User not found!"})
+  }
 
-    request.user = user
+  request.user = user;
 
-    return next()
-}
+  return next();
+} 
 
 app.post('/users', (request, response) => {
-  const { name, username } = request.body;
+  const { name, username } = request.body
 
-  const usernameAlreadyExists = users.some((user) => user.username === username);
+  const userAlreadyExists = users.find(user => user.username === username);
 
-  if (usernameAlreadyExists) {
-    return response.status(400).json({ error: 'Username already exists' });
+  if(userAlreadyExists){
+    return response.status(400).json({error:'User already exists'})
   }
 
   const user = {
     id: uuidv4(),
     name,
     username,
-    pro: false,
-    todos: []
+    todos:[],
   };
 
-  users.push(user);
+  users.push(user)
 
   return response.status(201).json(user);
 });
@@ -53,50 +53,67 @@ app.get('/todos', checksExistsUserAccount, (request, response) => {
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  const { title, deadline } = request.body;
   const { user } = request;
+  const { title, deadline } = request.body
 
-  const createTodo = {
+  const todoOperation = {
     id: uuidv4(),
     title,
     done: false,
     deadline: new Date(deadline),
     created_at: new Date()
-  };
+  }
+  
+  user.todos.push(todoOperation);
 
-  user.todos.push(createTodo);
-
-  return response.status(201).json(createTodo);
+  return response.status(201).json(todoOperation)
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const { title, deadline } = request.body;
-  const { todo } = request;
+  const { user } = request;
+  const { title, deadline } = request.body
+  const { id } = request.params
 
-  todo.title = title;
-  todo.deadline = new Date(deadline);
+  const checkTodo = user.todos.find(todo => todo.id === id)
 
-  return response.json(todo);
+  if (!checkTodo){
+    return response.status(404).json({error: "Todo not found!"})
+  }
+
+  checkTodo.title = title,
+  checkTodo.deadline = deadline
+
+  return response.json(checkTodo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  const { todo } = request;
+  const { user } =request;
+  const { id } = request.params
 
-  todo.done = true;
+  const checkTodo = user.todos.find(todo => todo.id === id);
 
-  return response.json(todo);
+  if (!checkTodo){
+    return response.status(404).json({error: "Todo not found!"})
+  }
+
+  const newDone = true
+
+  checkTodo.done = newDone;
+
+  return response.json(checkTodo)
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  const { user, todo } = request;
+  const { user } =request
+  const { id } = request.params
 
-  const todoIndex = user.todos.indexOf(todo);
+  const todoIndex = user.todos.findIndex(todo => todo.id === id); //findIndex retorna a posição no array que o objeto está localizado
 
-  if (todoIndex === -1) {
-    return response.status(404).json({ error: 'Todo not found' });
+  if (todoIndex === -1){
+    return response.status(404).json({error: "Todo not found!"})
   }
 
-  user.todos.splice(todoIndex, 1);
+  user.todos.splice(todoIndex, 1) //Primeiro Argumento: Por onde quero comecar a excluir; Segundo argumento: quantas posições quero excluir após o inicio
 
   return response.status(204).send();
 });
